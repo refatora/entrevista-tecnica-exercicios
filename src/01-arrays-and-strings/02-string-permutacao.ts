5c070bc9d59fe43d78e6b75724ae2784c8bac9cd
Refatora <refatora2024@gmail.com>
2025-10-02 22:30:33 -0400

    style: format

--- src/01-arrays-and-strings/02-string-permutacao.ts
+++ src/01-arrays-and-strings/02-string-permutacao.ts
@@ -1,5 +1,9 @@
 export function isPermutation(a: string, b: string): boolean {
-  const aSorted = Array.from(a).sort((left,right) => left.localeCompare(right)).toString()
-  const bSorted = Array.from(b).sort((left,right) => left.localeCompare(right)).toString()
+  const aSorted = Array.from(a)
+    .sort((left, right) => left.localeCompare(right))
+    .toString()
+  const bSorted = Array.from(b)
+    .sort((left, right) => left.localeCompare(right))
+    .toString()
   return aSorted === bSorted
 }
