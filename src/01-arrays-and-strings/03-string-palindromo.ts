44b06effaa4b82a7694f4a52a2e7cc3612be6c5c
Refatora <refatora2024@gmail.com>
2025-10-03 20:08:37 -0400

    doc: comentarios no exercicio do palindromo

--- src/01-arrays-and-strings/03-string-palindromo.ts
+++ src/01-arrays-and-strings/03-string-palindromo.ts
@@ -1,3 +1,28 @@
+/**
+ * --- Enunciado do exercicio ---
+ *
+ * Determinar se uma string é um palíndromo.
+ * Um palíndromo é uma palavra, frase, número ou outra sequência de caracteres
+ * que se lê da mesma forma para a frente e para trás.
+ */
+
+/**
+ * Abordagem com Dois Ponteiros (Two Pointers):
+ *
+ * Esta função utiliza a técnica de dois ponteiros para verificar se a string é um palíndromo
+ * de forma eficiente, comparando as extremidades da string e movendo-se em direção ao centro.
+ *
+ * 1. Ponteiro `left`: Inicia no primeiro caractere (índice 0).
+ * 2. Ponteiro `right`: Inicia no último caractere (índice `str.length - 1`).
+ *
+ * O loop continua enquanto `left` for menor que `right`:
+ * - A cada iteração, os caracteres nas posições `left` e `right` são comparados.
+ * - Se os caracteres forem diferentes, a string não é um palíndromo, e a função retorna `false`.
+ * - Se forem iguais, os ponteiros são movidos para o centro: `left` é incrementado e `right` é decrementado.
+ *
+ * Se o loop terminar sem encontrar caracteres diferentes, significa que a string é um palíndromo,
+ * e a função retorna `true`.
+ */
 export function isPalindromeTwoPointers(str: string): boolean {
   let left = 0
   let right = str.length - 1
