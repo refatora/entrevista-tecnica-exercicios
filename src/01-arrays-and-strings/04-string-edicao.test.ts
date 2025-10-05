b8adbcd8a080517e773b3cd6c53b8ee4599897ee
Refatora <refatora2024@gmail.com>
2025-10-05 16:31:51 -0400

    feat: refazer string edicao

--- src/01-arrays-and-strings/04-string-edicao.test.ts
+++ src/01-arrays-and-strings/04-string-edicao.test.ts
@@ -2,60 +2,52 @@ import { describe, expect, it } from 'vitest'
 import { hasEdits } from './04-string-edicao'
 
 describe(`exercicio-04: Determina se uma string tem 0 ou 1 edicao`, () => {
-  it.skip.each([
-    [' ', ' '],
-    ['1', '1'],
-    ['11', '11'],
-    ['111', '111'],
-    ['123', '123'],
+  it.each([
+    ['a', 'a'],
+    ['ab', 'ab'],
     ['abc', 'abc'],
   ])('retorne 0 quando as duas strings sao iguais: [%s, %s]', (a, b) => {
     expect(hasEdits(a, b)).toBe(0)
   })
 
-  it.skip.each([
-    [' ', '0'],
-    ['1', '0'],
-    ['11', '01'],
-    ['11', '10'],
-    ['111', '011'],
-    ['111', '101'],
-    ['111', '110'],
+  it.each([
+    ['a', 'x'],
+    ['ab', 'xb'],
+    ['ab', 'ax'],
+    ['abc', 'xbc'],
+    ['abc', 'axc'],
+    ['abc', 'abx'],
   ])('retorne 1 quando a segund string tem um caractere substituido: [%s, %s]', (a, b) => {
     expect(hasEdits(a, b)).toBe(1)
   })
 
-  it.skip.each([
-    [' ', ''],
+  it.each([
     ['1', ''],
-    ['11', '1'],
-    ['111', '11'],
-    ['123', '23'],
-    ['123', '13'],
+    ['12', '2'],
+    ['12', '1'],
     ['123', '12'],
+    ['123', '13'],
+    ['123', '23'],
   ])('retorne 1 quando a segunda string tem um caractere removido: [%s, %s]', (a, b) => {
     expect(hasEdits(a, b)).toBe(1)
   })
 
-  it.skip.each([
-    ['', ' '],
-    ['', '1'],
-    ['1', '11'],
-    ['11', '111'],
-    ['23', '123'],
-    ['13', '123'],
-    ['12', '123'],
+  it.each([
+    ['', 'x'],
+    ['a', 'ax'],
+    ['a', 'xa'],
+    ['ab', 'xab'],
+    ['ab', 'axb'],
+    ['ab', 'abx'],
   ])('retorne 1 quando a segunda string tem um caractere adicionado: [%s, %s]', (a, b) => {
     expect(hasEdits(a, b)).toBe(1)
   })
 
   it.each([
-    // [' ', '12'],
-    // ['1', '00'],
-    // ['11', '00'],
-    // ['111', '1'],
-    // ['123', '03'],
-    ['1', '01'],
+    ['a', 'xxx'],
+    ['a', 'xxa'],
+    ['a', 'xx'],
+    ['abc', 'x'],
   ])('retorne -1 quando a segunda string tem 2 ou mais edicoes: [%s, %s]', (a, b) => {
     expect(hasEdits(a, b)).toBe(-1)
   })
--- src/01-arrays-and-strings/04-string-edicao.ts
+++ src/01-arrays-and-strings/04-string-edicao.ts
@@ -1,24 +1,38 @@
+function hasOneEdit(a: string, b: string): boolean {
+  if (a.length !== b.length) {
+    return false
+  }
+  const counts = new Array(a.length)
+    .fill(1)
+    .filter((_, i) => a[i] !== b[i])
+    .reduce((prev, curr) => prev + curr)
+  return counts === 1
+}
+
+function hasOneRemoved(a: string, b: string): boolean {
+  const target = Array.from(a)
+  let i = 0
+  do {
+    if (target[i] !== b[i]) {
+      target.splice(i, 1)
+    }
+    i += 1
+  } while (i < a.length)
+  return b === target.join('')
+}
+
 export function hasEdits(a: string, b: string): 0 | 1 | -1 {
   if (a === b) {
     return 0
   }
-  let replaced: Array<string> = []
-  let added: Array<string> = []
-  let removed: Array<string> = []
-
-  for (let i = 0; i < Math.max(a.length, b.length); i++) {
-    if (a[i] === b[i]) {
-      replaced[i] = a[i]
-      added[i] = a[i]
-      removed[i] = a[i]
-    } else {
-      replaced[i] = b[i]
-      added[i] = b[i]
-      removed[i] = a[i] ?? '' + b[i]
-    }
+  if (a.length === b.length) {
+    return hasOneEdit(a, b) ? 1 : -1
   }
-  const possibleEdits = [replaced.join(''), added.join(''), removed.join('')]
-
-  console.log({ a, b, replaced, added, removed })
-  return possibleEdits.find((edit) => edit === b) !== undefined ? 1 : -1
+  if (a.length === b.length + 1) {
+    return hasOneRemoved(a, b) ? 1 : -1
+  }
+  if (a.length === b.length - 1) {
+    return hasOneRemoved(b, a) ? 1 : -1
+  }
+  return -1
 }
