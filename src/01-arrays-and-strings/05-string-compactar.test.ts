8dacc6ac6ce6506c9a37709d23d2d28d9f76fd88
Refatora <refatora2024@gmail.com>
2025-10-15 16:56:53 -0400

    refactor: renomear vars em compactar string

--- src/01-arrays-and-strings/05-string-compactar.test.ts
+++ src/01-arrays-and-strings/05-string-compactar.test.ts
@@ -2,13 +2,6 @@ import { describe, expect, it } from 'vitest'
 import { compact } from './05-string-compactar'
 
 describe('exercicio-05: Compactar string', () => {
-  it.each(['a', 'abc', 'abcdefg', 'aa', 'aab', 'aabb', 'abbb'])(
-    'retorne a mesma string quando compactacao resultar em uma string de maior ou igual comprimento: [%s]',
-    (str) => {
-      expect(compact(str)).toBe(str)
-    },
-  )
-
   it.each([
     ['aaa', 'a3'],
     ['aaabbb', 'a3b3'],
--- src/01-arrays-and-strings/05-string-compactar.ts
+++ src/01-arrays-and-strings/05-string-compactar.ts
@@ -3,36 +3,33 @@
  *
  * Implemente um método para compactar uma string, baseando-se na contagem de caracteres repetidos em sequência.
  * Por exemplo, a string 'aabbcccdddde' se tornaria 'a2b2c3d4e1'.
- * Se a string "compactada" não for menor que a string original, seu método deve retornar a string original.
  */
 
 /**
  * Abordagem de Iteração e Contagem:
  *
  * Esta função itera pela string para contar caracteres consecutivos.
- * Ela mantém um `current` (caractere atual) e um `count` (contador).
- * Quando o caractere muda, o `current` e seu `count` são adicionados a um array `result`.
+ * Ela mantém um `prev` (caractere atual) e um `count` (contador).
+ * Quando o caractere muda, o `prev` e seu `count` são adicionados a um array `result`.
  * Ao final do loop, o último grupo de caracteres também é adicionado.
  * A string compactada é formada pela junção dos elementos do array `result`.
- * Por fim, a função compara o comprimento da string original com a compactada e retorna a menor delas.
  */
 export function compact(str: string) {
   let result: Array<string | number> = []
-  let current: string = str[0]
+  let prev: string = str[0]
   let count: number = 0
 
   for (let i = 0; i <= str.length; i++) {
-    const char = str[i]
-    if (current === char) {
+    const current = str[i]
+    if (prev === current) {
       count++
     } else {
-      result.push(current)
+      result.push(prev)
       result.push(count)
-      current = char
+      prev = current
       count = 1
     }
   }
 
-  const resultStr = result.join('')
-  return str.length <= resultStr.length ? str : resultStr
+  return result.join('')
 }
