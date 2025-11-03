5afc39d7a681a596aa82bfd12dc1b72ede646d79
Refatora <refatora2024@gmail.com>
2025-11-02 22:07:51 -0500

    feat: rotacionar matriz quadrada dificil

--- src/01-arrays-and-strings/07-array-rotacionar-matriz-dificil.test.ts
+++ src/01-arrays-and-strings/07-array-rotacionar-matriz-dificil.test.ts
@@ -56,6 +56,24 @@ describe('exercicio-06: Rotacionar matriz em 90 graus (facil)', () => {
         [24, 19, 14, 9, 4],
       ],
     ],
+    [
+      [
+        [0, 1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10, 11],
+        [12, 13, 14, 15, 16, 17],
+        [18, 19, 20, 21, 22, 23],
+        [24, 25, 26, 27, 28, 29],
+        [30, 31, 32, 33, 34, 35],
+      ],
+      [
+        [30, 24, 18, 12, 6, 0],
+        [31, 25, 19, 13, 7, 1],
+        [32, 26, 20, 14, 8, 2],
+        [33, 27, 21, 15, 9, 3],
+        [34, 28, 22, 16, 10, 4],
+        [35, 29, 23, 17, 11, 5],
+      ],
+    ],
   ])('deve rotacionar a matriz em 90 graus: [%j, %j]', (given, expected) => {
     expect(rotateMatrixInPlaceHard(given)).toStrictEqual(expected)
   })
