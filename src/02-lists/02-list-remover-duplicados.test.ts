55e6c4d384e80e0f9724104384842d3ebaba28a2
Refatora <refatora2024@gmail.com>
2025-11-23 14:11:07 -0500

    refactor: limpar tests

--- src/02-lists/02-list-remover-duplicados.test.ts
+++ src/02-lists/02-list-remover-duplicados.test.ts
@@ -3,26 +3,14 @@ import { DelDuplicatesLinkedList } from './02-list-remover-duplicados'
 
 describe.skip('exercicio-02: Remova valores duplicados em uma lista encadeada', () => {
   it.each([
-    [[], []],
-    [[1], [1]],
-    [[1, 1], [1]],
-    [
-      [1, 1, 2],
-      [1, 2],
-    ],
-    [
-      [1, 2, 2],
-      [1, 2],
-    ],
-    [
-      [1, 2, 2, 2],
-      [1, 2],
-    ],
-    [
-      [1, 2, 1, 3, 2],
-      [1, 2, 3],
-    ],
-  ])('remover valores duplicados: [%s -> %s]', (init, expected) => {
+    { init: [], expected: [] },
+    { init: [1], expected: [1] },
+    { init: [1, 1], expected: [1] },
+    { init: [1, 1, 2], expected: [1, 2] },
+    { init: [1, 2, 2], expected: [1, 2] },
+    { init: [1, 2, 2, 2], expected: [1, 2] },
+    { init: [1, 2, 1, 3, 2], expected: [1, 2, 3] },
+  ])('remover valores duplicados: %j', ({ init, expected }) => {
     const list = new DelDuplicatesLinkedList(init)
     list.delDuplicates()
     expect(list.toArray()).toStrictEqual(expected)
